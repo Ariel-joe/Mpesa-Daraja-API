7f98cd8d45832d36010053f85490b0304b3a3fb0
Ariel-Joe <arieljoe00@gmail.com>
2025-06-04 14:48:36 +0300

    config authorization API

--- accessToken.js
+++ accessToken.js
@@ -1,27 +1,27 @@
 // generate tokens for authorization
 
+import axios from "axios";
+
 export const getTokenAccess = async () => {
   const consumerKey = process.env.MPESA_CONSUMER_KEY;
   const consumerSecret = process.env.CONSUMERSECRET;
+  const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
+    "base64"
+  ); //also know as encodedCredentials
 
-  const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  const url =
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
   // url for live
   // const url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
   try {
-    const encodedCredentials = new Buffer.from(
-      consumerKey + ":" + consumerSecret
-    ).toString("base64");
-
     const headers = {
-      Authorization: "Basic" + "" + encodedCredentials,
+      Authorization: `Basic ${auth}`,
       "Content-Type": "application/json",
     };
 
-    const response = await fetch(url, { headers });
-
-    console.log(response.data.access_token);
+    const response = await axios.get(url, { headers });
 
     return response.data.access_token;
   } catch (error) {
